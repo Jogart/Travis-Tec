7b8982ded19cf5791cef2450b6ea918a17b1bdba
Antonio <tsu.bedoya@gmail.com>
2018-09-07 17:33:02 -0500

    Cdfdf:wq
    C
    B
    C
    
    B
    
    D
    [ADD] tec.py: Add filevis yml file
    
    B
    
    A
    A
    B
    B
    B

---
+++ tec.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+suma=4+6
+
+def resultado(suma):
+    if suma == 10:
+        print('La suma es correcta, es %s') % str(suma)
+    else:
+        print('La suma es igual a %s') %str(suma)
+    return suma
